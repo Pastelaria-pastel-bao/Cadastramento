0c050171e428193c8137d6e820a138b3f14c4a86
joaolenia <127161450+joaolenia@users.noreply.github.com>
2024-07-02 21:35:11 -0300

    Feat: Adicionando repositorio

---
+++ src/main/java/com/cadastro/cadastramento/repository/PasteisRepository.java
@@ -0,0 +1,10 @@
+package com.cadastro.cadastramento.repository;
+
+import com.cadastro.cadastramento.entity.Pasteis;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface PasteisRepository extends JpaRepository<Pasteis, Long> {
+    Optional<Pasteis> findByTitulo(String titulo);
+
+}
